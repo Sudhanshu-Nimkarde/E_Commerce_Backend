487d2bf8ad2047dedb328c5c9fd7dea968c53dd2
Sush149 <kawdes694@gmail.com>
2026-05-21 18:20:15 +0530

    Add user migration and factory changes

--- database/migrations/2025_05_14_170617_add_columns_to_users_table.php
+++ database/migrations/2025_05_14_170617_add_columns_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('contact')->after('email')->nullable();
             $table->string('gender')->after('contact')->nullable();
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
